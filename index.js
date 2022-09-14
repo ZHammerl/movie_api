66e1b8f32b6e12526a8407bb90ce3d9ac93421b4
ZHammerl <108287700+ZHammerl@users.noreply.github.com>
2022-09-14 16:05:56 +0200

    CORS implemented

--- index.js
+++ index.js
@@ -41,21 +41,22 @@ const requestTime = (req, res, next) => {
 const cors = require('cors');
 app.use(cors()); // Option 1: allow all domains
 // Option 2: only allow domains in 'allowed origins'
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         // If a specific origin isn’t found on the list of allowed origins
-//         let message =
-//           'The CORS policy for this application doesn"t allow access from origin ' + origin;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix-app22.netlify.app'];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          'The CORS policy for this application doesn"t allow access from origin ' + origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 // use Middleware
 app.use(morgan('combined', { stream: accessLogStream }));
